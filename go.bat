19659878b77666fa76338e1ea4965e0ead6cc059
J█████ <dev.nullinside@gmail.com>
2021-07-11 15:02:32 -0400

    [Bug] Fixing script not skipping when there isn't a tag

--- go.bat
+++ go.bat
@@ -3,13 +3,13 @@ echo off
 :: Get the version number
 FOR /F "tokens=* USEBACKQ" %%F IN (`git tag --points-at HEAD`) DO (SET version=%%F)
 
-if [%version]==[] (
+if [%version%]==[] (
 	echo This build doesn't have a tag
 	exit 1
 )
 
 echo on
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" streaming-tools/streaming-tools.sln /t:Clean,Restore,Build /p:Configuration=Release /p:Platform="Any CPU"
+::call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" streaming-tools/streaming-tools.sln /t:Clean,Restore,Build /p:Configuration=Release /p:Platform="Any CPU"
 xcopy streaming-tools\streaming-tools\bin\Release\net5.0 "v%version%\streaming-tools\" /s /e
 xcopy streaming-tools\WindowsKeyboardHook\bin\Release\net5.0 "v%version%\WindowsKeyboardHook\" /s /e
 
